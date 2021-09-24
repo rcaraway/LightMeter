cd117023440ca99a1b598b935794c42c4a64f615
Rob Caraway <robbor911@gmail.com>
2021-09-24 17:42:17 -0500

    camera showing and basic views setup

--- lib/main.dart
+++ lib/main.dart
@@ -44,11 +44,23 @@ class CameraPage extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           return Stack(children: [
             CameraPreview(_controller),
+            Center(
+                child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.transparent,
+                        border: Border.all(width: 2, color: Colors.white)))),
             Align(
                 alignment: Alignment.bottomCenter,
                 child: Container(
                   width: Get.width,
                   height: Get.height * 0.20,
+                  color: Colors.transparent,
+                  child: const Center(
+                    child: Text('Test'),
+                  ),
                 ))
           ]);
         } else {
